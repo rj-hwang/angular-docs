3b9fc909325b4de587accf96a35801f3b82d963e
dragon <rongjihuang@gmail.com>
2014-07-02 14:56:56 +0800

    update to 1.2.19

--- docs/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');  
-var connect = require('connect');  
-var app = connect(); 
-var port = 8080;
-app.use(connect.static(__dirname + '/'));
-http.createServer(app).listen(port);
-console.log('httpServer at http://localhost:' + port);
